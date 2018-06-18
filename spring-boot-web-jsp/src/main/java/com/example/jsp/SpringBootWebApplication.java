5cbdbcccfb4455750eee1e0d3ce37bb63c9ceb56
pradeep reddy <pradeepkadati@gmail.com>
2018-06-18 20:12:26 +0530

    Updated Employee Form and git ignore

--- spring-boot-web-jsp/src/main/java/com/example/jsp/SpringBootWebApplication.java
+++ spring-boot-web-jsp/src/main/java/com/example/jsp/SpringBootWebApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.emp")
 public class SpringBootWebApplication extends SpringBootServletInitializer {
 
 	@Override
